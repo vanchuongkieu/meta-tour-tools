c6a9bfba3f5b9666d5688449f8ea226cd646d5be
vanchuongkieu <vanchuongkieu@gmail.com>
2022-07-27 23:01:04 +0700

    dev test 15

--- src/pages/abc.tsx
+++ src/pages/abc.tsx
@@ -1,7 +1,13 @@
 type Props = {};
 
 const abc = (props: Props) => {
-  return <iframe src="http://localhost:3000/" width={800} height={400} />;
+  return (
+    <iframe
+      src="https://meta-tour-tools.vercel.app/"
+      width={800}
+      height={400}
+    />
+  );
 };
 
 export default abc;
